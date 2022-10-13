0f2f0d62c63784f460aeab7d1325ab341947821a
kyuminKim <kyuminkim0122@gmail.com>
2022-10-14 00:50:48 +0900

    add game base frame
    게임 기본 틀 추가

--- Final_Project/main.py
+++ Final_Project/main.py
@@ -1,72 +1,18 @@
-from pico2d import *
-import random
+import play_state
+import pico2d
+import title_state
 
-class Dragon:
-    def __init__(self):
-        self.x, self.y = random.randint(100, 700),100
-        self.frame = 0
-        self.image = load_image('source/image/flying_dragon-red.png')
-
-    def update(self):
-        self.frame = (self.frame + 1) % 3
-        pass
-
-    def draw(self):
-        self.image.clip_draw(self.frame * 144, 128 * 3, 144, 128, self.x, self.y)
-        pass
+start_state = [title_state, play_state]
+pico2d.open_canvas()
 
 
-class Field:
-    # def __init__(self):
-    #     self.image = load_image('')
-    pass
+for state in start_state:
+    state.enter()  # initialize
+    # game loop
+    while state.running:
+        state.handle_events()
+        state.update()
+        state.draw()
+    state.exit()   # exit
 
-class Enemy:
-    pass
-
-
-def handle_events():
-    global running
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.type == SDLK_ESCAPE:
-                running = False
-            elif event.type == SDLK_w:  # up
-                pass
-            elif event.type == SDLK_a:  # left
-                pass
-            elif event.type == SDLK_s:  # down
-                pass
-            elif event.type == SDLK_d:  # right
-                pass
-        elif event.type == SDL_KEYUP:
-            if event.type == SDLK_w:    # up
-                pass
-            elif event.type == SDLK_a:  # left
-                pass
-            elif event.type == SDLK_s:  # down
-                pass
-            elif event.type == SDLK_d:  # right
-                pass
-
-
-
-
-open_canvas(800, 950)
-dragon = Dragon()
-
-running = True
-
-while running:
-    handle_events()
-    dragon.update()
-
-    clear_canvas()
-    dragon.draw()
-    update_canvas()
-    delay(0.15)
-
-close_canvas()
+pico2d.close_canvas()
